916b8e139b61dfbf1b9b68373b7dd789de7044b2
Gaurav Bhardwaj <myselfgroot@gmail.com>
2020-07-05 19:57:47 +0530

    Updates

---
+++ precache-manifest.daeef6c3220f463ab309436b82fe109a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "12a8c04ca7a4830423d17470248da4e0",
+    "url": "/monster-search/index.html"
+  },
+  {
+    "revision": "b224ce78dde8718e9d76",
+    "url": "/monster-search/static/css/main.931a46b0.chunk.css"
+  },
+  {
+    "revision": "466c25b76239dcf318a9",
+    "url": "/monster-search/static/js/2.19df0d7f.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster-search/static/js/2.19df0d7f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b224ce78dde8718e9d76",
+    "url": "/monster-search/static/js/main.ef0f2dce.chunk.js"
+  },
+  {
+    "revision": "61be7456de1d7dabeada",
+    "url": "/monster-search/static/js/runtime-main.7fba5a41.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monster-search/static/media/logo.5d5d9eef.svg"
+  }
+]);
